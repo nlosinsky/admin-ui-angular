1834bae76ca877d000e3f1fa2af265621fdcaad7
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-03-25 18:46:58 +0200

    changed guards according to new syntax

--- src/app/auth/routes.ts
+++ src/app/auth/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { NoAuthGuard } from '@app/auth/guards/no-auth.guard';
+import { noAuthGuard } from '@app/auth/guards/no-auth.guard';
 import { LoginComponent } from '@app/auth/login/login.component';
 
 export const ROUTES: Routes = [
   {
     path: '',
-    canActivate: [NoAuthGuard],
+    canActivate: [noAuthGuard],
     children: [
       {
         path: 'login',
